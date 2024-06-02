34dc9fae59251c87c6ad1c3929b131cf54c7a1e4
Diego Lopes <diegopereiralopes98@gmail.com>
2024-06-02 17:03:04 -0300

    feat: return 200 for one request

--- app/server.go
+++ app/server.go
@@ -13,9 +13,17 @@ func main() {
 		os.Exit(1)
 	}
 
-	_, err = l.Accept()
+	c, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+
+	handleConn(c)
+}
+
+func handleConn(c net.Conn) {
+	defer c.Close()
+
+	c.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
 }
