af8235211614d406e184c61d4a56b6e1fadb160e
Diego Lopes <diegopereiralopes98@gmail.com>
2024-06-02 17:43:59 -0300

    feat: return with body for /echo

--- app/server.go
+++ app/server.go
@@ -45,13 +45,30 @@ func handleConn(c net.Conn) {
 
 	path := strings.Split(lines[0], SEPARATOR)[1]
 
+	fmt.Println(path)
+
 	var response string
 
 	if path == "/" {
 		response = fmt.Sprintf("HTTP/1.1 200 OK%s%s", CRLF, CRLF)
+	} else if strings.HasPrefix(path, "/echo/") {
+		arg := strings.Split(path, "/")[2]
+		response = echo(arg)
 	} else {
 		response = fmt.Sprintf("HTTP/1.1 404 Not Found%s%s", CRLF, CRLF)
 	}
 
 	c.Write([]byte(response))
+}
+
+func echo(arg string) string {
+	response := fmt.Sprintf("HTTP/1.1 200 OK%s", CRLF)
+
+	response += fmt.Sprintf("Content-Type: text/plain%s", CRLF)
+	response += fmt.Sprintf("Content-Length: %d%s", len(arg), CRLF)
+	response += CRLF
+
+	response += arg
+
+	return response
 }
